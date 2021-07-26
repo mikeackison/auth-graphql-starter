4fc2e5ff3127ed8f14e7a40663448f490de7da62
Mike Ackison <mike.ackison@shopify.com>
2021-07-26 18:45:18 -0400

    current user query

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+import CurrentUser from '../queries/CurrentUser';
 
 class Header extends Component {
   render() {
+    console.log(this.props.data);
+
     return <div>Header</div>;
   }
 }
 
-export default Header;
+export default graphql(CurrentUser)(Header);
